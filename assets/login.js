1a41e83782bdf9a19394e4a8063bb6abb5481023
enissaosmanovic <eniosmanovic@outlook.fr>
2024-02-22 20:06:16 +0100

    javacrip pag d'acceuil

--- assets/login.js
+++ assets/login.js
@@ -1,24 +1,63 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('login-form');
+    const errorMessage = document.getElementById('error-message');
+    const loginLink = document.querySelector('#nav-login');
 
+    // Vérifier si le token est présent dans le localStorage
+    const token = sessionStorage.getItem('token');
 
-loginForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    await login(); // Appel de la fonction de connexion
-});
-
-// Fonction asynchrone pour gérer la connexion
-const login = async () => {
-    try {
-        // Récupération des informations du formulaire
-        const userInfo = {
-            email: inputMail.value,
-            password: inputPassword.value
-        };
+    if (token) {
+        // Changer le texte en "logout" si le token est présent
+        loginLink.textContent = 'logout';
     }
-}
-const response = await fetch("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(userInfo)
+
+    loginForm.addEventListener('submit', async function (event) {
+        event.preventDefault();
+
+        const emailInput = document.getElementById('email');
+        const passwordInput = document.getElementById('password');
+
+        const email = emailInput.value;
+        const password = passwordInput.value;
+
+        try {
+            const response = await fetch('http://localhost:5678/api/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error('Erreur lors de la connexion');
+            }
+
+            const data = await response.json();
+            const token = data.token;
+
+            // Stocker le token dans le localStorage
+            sessionStorage.setItem('token', token);
+            console.log('Token:', token);
+
+            // Changer le texte du lien en "logout" après la connexion réussie
+            loginLink.textContent = 'logout';
+
+            // Rediriger vers une autre page ou effectuer d'autres actions
+            window.location.href = 'index.html';
+        } catch (error) {
+            console.error(error);
+            errorMessage.removeAttribute('hidden');
+        }
+    });
+
+    // Gérer la déconnexion lorsque l'utilisateur clique sur "logout"
+    loginLink.addEventListener('click', function () {
+        if (token) {
+            sessionStorage.removeItem('token');
+        }
+    });
 });
--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,65 @@
+document.addEventListener("DOMContentLoaded", async (event) => {
+    let urlWorks = "http://localhost:5678/api/works";
+    let urlCategories = "http://localhost:5678/api/categories";
+    let allimages;
+    let modal = null;
 
+    async function getCategories() {
+        try {
+            const response = await fetch(urlCategories);
 
-async function getCategories() {
-    try {
-        // Récupération des catégories depuis le fichier JSON
-        const response = await fetch('http://localhost:5678/api/categories');
+            if (!response.ok) {
+                throw new Error('Erreur de requête ' + response.status);
+            }
 
-        // Vérifier si la requête a réussi (statut OK)
-        if (!response.ok) {
-            throw new Error('Erreur de requête ' + response.status);
+            const gallery = document.querySelector('.gallery');
+            const categoriesJson = await response.json();
+            gallery.innerHTML = ''; // Efface la galerie existante
+
+            console.log(categoriesJson);
+
+            const filtres = document.querySelector('.categories');
+
+            categoriesJson.forEach(category => {
+                let unFiltre = document.createElement('a');
+                unFiltre.classList.add('btn-filtre');
+                unFiltre.innerText = category.name;
+                unFiltre.addEventListener('click', () => filterImagesByCategory(category.id));
+                filtres.appendChild(unFiltre);
+            });
+
+            const tousBtn = document.getElementById('Tousbtn');
+            console.log(tousBtn);
+            tousBtn.addEventListener('click', () => {
+                console.log('Bouton Tous cliqué');
+                imgfiltre();
+            });
+
+        } catch (error) {
+            console.error('Une erreur s\'est produite :', error);
         }
-
-        const categoriesJson = await response.json();
-
-        console.log(categoriesJson);
-
-        const filtres = document.querySelector('.categories');
-
-
-        categoriesJson.forEach(category => {
-            let unFiltre = document.createElement('button');
-            unFiltre.classList.add('btn');
-            unFiltre.innerText = category.name;
-            unFiltre.setAttribute('id', category.id)
-            filtres.appendChild(unFiltre);
-
-        });
-    } catch (error) {
-        console.error('Une erreur s\'est produite :', error);
     }
-}
 
+    async function getImages() {
+        try {
+            const images = await fetch(urlWorks);
+            const imagesJson = await images.json();
 
-async function getImages() {
-    try {
-        const images = await fetch('http://localhost:5678/api/works');
-        const imagesJson = await images.json();
+            console.log(imagesJson);
 
-        console.log(imagesJson);
+            const gallery = document.querySelector('.gallery');
+            allimages = imagesJson;
+            imgfiltre(); // Appel de la fonction pour afficher toutes les images
+        } catch (error) {
+            console.error('Une erreur s\'est produite lors de la récupération des images :', error);
+        }
+    }
 
+    async function imgfiltre() {
         const gallery = document.querySelector('.gallery');
+        gallery.innerHTML = '';
 
-        imagesJson.forEach(uneImage => {
+        allimages.forEach(uneImage => {
             const figure = document.createElement('figure');
             const imageElement = document.createElement('img');
             const figcaption = document.createElement('figcaption');
@@ -54,12 +73,85 @@ async function getImages() {
 
             gallery.appendChild(figure);
         });
-    } catch (error) {
-        console.error('Une erreur s\'est produite lors de la récupération des images :', error);
     }
-}
 
-// Appeler la fonction pour récupérer les catégories //
-getCategories();
-getImages();
+    async function filterImagesByCategory(categoryId) {
+        const gallery = document.querySelector('.gallery');
+        gallery.innerHTML = ''; // Efface la galerie existante
 
+        const filteredimages = allimages.filter(image => image.categoryId == categoryId);
+
+        filteredimages.forEach(uneImage => {
+            const figure = document.createElement('figure');
+            const imageElement = document.createElement('img');
+            const figcaption = document.createElement('figcaption');
+
+            imageElement.src = uneImage.imageUrl;
+            imageElement.alt = uneImage.title;
+            figcaption.innerText = uneImage.title;
+
+            figure.appendChild(imageElement);
+            figure.appendChild(figcaption);
+
+            gallery.appendChild(figure);
+        });
+    }
+
+    const openmodal = function (e, type = "photos") {
+        e.preventDefault();
+        const target = document.querySelector(e.currentTarget.getAttribute('href'));
+
+        // Gérer le fait de récupérer le bon contenu du modal
+        if (type == "photos") {
+            getImagesmodal(target);
+        }
+
+        target.style.display = 'flex';
+        target.style.position = 'fixed';
+        target.setAttribute('aria-modal', 'true');
+        modal = target;
+        modal.addEventListener('click', closemodal);
+
+    };
+
+    const closemodal = function (e) {
+        e.preventDefault();
+        if (modal) {
+            modal.style.display = 'none';
+            modal.removeAttribute('aria-modal');
+            modal.removeEventListener('click', closemodal);
+            modal = null;
+        }
+    };
+
+    document.querySelectorAll('.js-modal').forEach(a => {
+        a.addEventListener('click', function (e) {
+            console.log('Clic sur un élément .js-modal');
+            openmodal(e);
+        });
+    });
+
+    async function getImagesmodal(target) {
+        try {
+            const imagesResponse = await fetch(urlWorks);
+            const imagesJson = await imagesResponse.json();
+
+            const modalContent = target.querySelector('.modal-img');
+            modalContent.innerHTML = ''; // Efface le contenu existant du modal
+
+            imagesJson.forEach(image => {
+
+                const imgElement = document.createElement('img');
+                imgElement.src = image.imageUrl;
+                imgElement.alt = image.title;
+                modalContent.appendChild(imgElement);
+            });
+        } catch (error) {
+            console.error('Une erreur s\'est produite lors de la récupération des images pour la modale :', error);
+        }
+    }
+
+
+});
+
+/* Fonction à appeler s =i ya un token*/
